69b0fbeb62db8c95c197330911c4469cdd0ee6cb
Jaime Munoz <jaimungon@gmail.com>
2021-02-12 18:03:27 +0100

    [add] form sell page

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,54 @@
+import useForm from '../lib/useForm'
+
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    name: 'Nice shoes',
+    price: 100,
+    description: 'those shoes are the best',
+  })
+
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="description">
+        Description
+        <input
+          type="textarea"
+          id="description"
+          name="description"
+          placeholder="Description"
+          value={inputs.description}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="button" onClick={clearForm}>
+        Clear Form
+      </button>
+      <button type="button" onClick={resetForm}>
+        Reset Form
+      </button>
+    </form>
+  )
+}
---
+++ frontend/lib/useForm.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+export default function useForm(initial = {}) {
+  //create stete object for inputs
+  const [inputs, setInputs] = useState(initial)
+
+  function handleChange(e) {
+    const { value, name, type } = e.target
+    if (type === 'number') {
+      value = parseInt(value)
+    }
+    if (type === 'file') {
+      value[0] = e.target.files
+    }
+    setInputs({
+      ...inputs,
+      [name]: value,
+    })
+  }
+
+  function resetForm() {
+    setInputs(initial)
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    )
+    setInputs(blankState)
+  }
+
+  return { inputs, handleChange, resetForm, clearForm }
+}
