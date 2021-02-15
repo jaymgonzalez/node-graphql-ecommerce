d35257e974fbdd098ac315a5c6cb272d04eaa5da
Jaime Munoz <jaimungon@gmail.com>
2021-02-15 17:11:10 +0100

    [add] pagination

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -11,8 +11,6 @@ const DELETE_PRODUCT_MUTATION = gql`
 `
 
 function update(cache, payload) {
-  console.log(payload)
-  console.log('running the update fx after delete')
   cache.evict(cache.identify(payload.data.deleteProduct))
 }
 
